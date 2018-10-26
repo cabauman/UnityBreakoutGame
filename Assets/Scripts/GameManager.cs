e633efe1baccd2af85646bb371929c94daa86a56
cabauman <cab452005@hotmail.com>
2018-10-26 12:41:59 +0900

    Position walls at edges of screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,4 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Object References")]
+    [SerializeField]
+    private Ball _ball;
+    [SerializeField]
+    private Transform _leftWall;
+    [SerializeField]
+    private Transform _rightWall;
+    [SerializeField]
+    private Transform _topWall;
+
+    private void Start()
+    {
+        _leftWall.position = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0.5f));
+        _rightWall.position = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0.5f));
+        _topWall.position = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1f));
+    }
 }
