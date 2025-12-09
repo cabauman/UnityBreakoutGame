3a11118e817421ea7af498cbd1a807787de602c8
cabauman <6819362+cabauman@users.noreply.github.com>
2025-12-09 15:43:05 +0900

    Add Localization package and apply to hud

--- Assets/Scripts/UI/Hud.cs
+++ Assets/Scripts/UI/Hud.cs
@@ -1,24 +1,47 @@
 ﻿using TMPro;
 using R3;
 using UnityEngine;
+using UnityEngine.Localization;
 
 namespace BreakoutGame
 {
     public sealed class Hud : MonoBehaviour
     {
-        private const string NUM_LIVES_FMT = "Lives: {0}";
-
         [SerializeField]
         private GamePresenter _gamePresenter;
         [SerializeField]
         private TextMeshProUGUI _numLivesLabel;
+        [SerializeField]
+        private LocalizedString livesString;
+
+        void OnEnable()
+        {
+            livesString.Arguments = new object[] { 0 };
+            livesString.StringChanged += UpdateString;
+        }
+
+        void OnDisable()
+        {
+            livesString.StringChanged -= UpdateString;
+        }
 
         private void Start()
         {
             _gamePresenter.Game
                 .NumLives
-                .Subscribe(numLives => _numLivesLabel.text = string.Format(NUM_LIVES_FMT, numLives))
+                .Subscribe(UpdateLives)
                 .AddTo(this);
+        }
+
+        private void UpdateLives(uint lives)
+        {
+            livesString.Arguments[0] = lives;
+            livesString.RefreshString();
+        }
+
+        private void UpdateString(string localizedString)
+        {
+            _numLivesLabel.text = localizedString;
         }
     }
 }
